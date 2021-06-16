ceb72be026271941d8f38410bc8882b4feb10c5e
illyasavanevich <illya.savanevich.work@gmail.com>
2021-06-16 14:59:49 +0300

    language

--- src/Utils/i18n.js
+++ src/Utils/i18n.js
@@ -3,10 +3,10 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-const Languages = ['en', 'ua'];
+const Languages = ['en', 'ga'];
 
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    fallbackLng: 'ua',
+    fallbackLng: 'en',
     debug: false,
     detection:{
         order: ['queryString', 'cookie'],
